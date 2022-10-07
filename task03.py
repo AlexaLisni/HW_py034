c03a087bfd0d9effbc8969cb136b85e9abdf6842
Alexandr <sashylay0@gmail.com>
2022-10-07 15:29:35 +0300

    finish task03

--- task03.py
+++ task03.py
@@ -1,12 +1,13 @@
 #Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов 
 #исходной последовательности в том же порядке.
-from random import sample
-
-
-a = [int(s) for s in input().split()]
-for i in range(len(a)):
-    for j in range(len(a)):
-        if i != j and a[i] == a[j]:
+from random import randint
+list = [randint(1, 10) for i in range(10)]
+print(list)
+res = []
+for i in range(len(list)):
+    for j in range(len(list)):
+        if i != j and list[i] == list[j]:
             break
     else:
-        print(a[i], end=' ')
+       res.append(list[i])
+print(res)
